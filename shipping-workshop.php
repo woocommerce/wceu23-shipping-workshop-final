05c5e778c200734dbaadf749420c800bacb18f6a
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-22 15:31:39 +0000

    Invoke init of store endpoint extensions class when wc blocks loads

--- shipping-workshop.php
+++ shipping-workshop.php
@@ -21,7 +21,12 @@ define( 'SHIPPING_WORKSHOP_VERSION', $plugin_data['version'] );
  * Include the dependencies needed to instantiate the block.
  */
 add_action('woocommerce_blocks_loaded', function() {
-    require_once __DIR__ . '/shipping-workshop-blocks-integration.php';
+    require_once __DIR__ . '/shipping-workshop-extend-store-endpoint.php';
+		require_once __DIR__ . '/shipping-workshop-blocks-integration.php';
+
+		// Initialize our store endpoint extension when WC Blocks is loaded.
+	Shipping_Workshop_Extend_Store_Endpoint::init();
+
 	add_action(
 		'woocommerce_blocks_checkout_block_registration',
 		function( $integration_registry ) {
