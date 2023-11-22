144f022f66d5b92580e6788bad7f8b53683c6309
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-22 15:31:47 +0000

    Fix linting errors

--- shipping-workshop.php
+++ shipping-workshop.php
@@ -20,33 +20,39 @@ define( 'SHIPPING_WORKSHOP_VERSION', $plugin_data['version'] );
 /**
  * Include the dependencies needed to instantiate the block.
  */
-add_action('woocommerce_blocks_loaded', function() {
-    require_once __DIR__ . '/shipping-workshop-extend-store-endpoint.php';
+add_action(
+	'woocommerce_blocks_loaded',
+	function() {
+		require_once __DIR__ . '/shipping-workshop-extend-store-endpoint.php';
 		require_once __DIR__ . '/shipping-workshop-blocks-integration.php';
 
 		// Initialize our store endpoint extension when WC Blocks is loaded.
-	Shipping_Workshop_Extend_Store_Endpoint::init();
+		Shipping_Workshop_Extend_Store_Endpoint::init();
 
-	add_action(
-		'woocommerce_blocks_checkout_block_registration',
-		function( $integration_registry ) {
-			$integration_registry->register( new Shipping_Workshop_Blocks_Integration() );
-		}
-	);
-});
+		add_action(
+			'woocommerce_blocks_checkout_block_registration',
+			function( $integration_registry ) {
+				$integration_registry->register( new Shipping_Workshop_Blocks_Integration() );
+			}
+		);
+	}
+);
 
 /**
  * Registers the slug as a block category with WordPress.
+ *
+ * @param array $categories Existing block categories.
+ * @return array
  */
 function register_Shipping_Workshop_block_category( $categories ) {
-    return array_merge(
-        $categories,
-        [
-            [
-                'slug'  => 'shipping-workshop',
-                'title' => __( 'Shipping_Workshop Blocks', 'shipping-workshop' ),
-            ],
-        ]
-    );
+	return array_merge(
+		$categories,
+		array(
+			array(
+				'slug'  => 'shipping-workshop',
+				'title' => __( 'Shipping_Workshop Blocks', 'shipping-workshop' ),
+			),
+		)
+	);
 }
 add_action( 'block_categories_all', 'register_Shipping_Workshop_block_category', 10, 2 );
